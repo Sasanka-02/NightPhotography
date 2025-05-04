9e38ab24439a5a415b57aaa2f8ea89ce660950d5
Sasanka-02 <sithirasasanka002@gmail.com>
2025-05-04 13:23:08 +0530

    Update NotificationException.java

--- NIGHTLIFE/src/main/java/com/zos/exception/NotificationException.java
+++ NIGHTLIFE/src/main/java/com/zos/exception/NotificationException.java
@@ -1,13 +1,2 @@
-package com.zos.exception;
 
-public class NotificationException extends Exception {
-
-    public NotificationException() {
-        super();
-    }
-
-    public NotificationException(String message) {
-        super(message);
-    }
-}
 
