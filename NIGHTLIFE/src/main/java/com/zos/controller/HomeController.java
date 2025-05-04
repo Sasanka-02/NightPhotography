fc8074b676d78792aa672ad2e6bc1acb6a01399a
Sasanka-02 <sithirasasanka002@gmail.com>
2025-05-04 13:18:23 +0530

    Update HomeController.java

--- NIGHTLIFE/src/main/java/com/zos/controller/HomeController.java
+++ NIGHTLIFE/src/main/java/com/zos/controller/HomeController.java
@@ -1,16 +1,1 @@
-package com.zos.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-	
-	@GetMapping("/api")
-	public String homeControllerHandler() {
-		
-		return "welcome to cookinghub backend api";
-		
-	}
-
-}
