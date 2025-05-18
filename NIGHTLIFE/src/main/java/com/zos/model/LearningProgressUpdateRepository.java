3f2f5557b6973fb539eb0102d44006e457c98635
Chathuni Nimesha <chathuni889@gmail.com>
2025-05-18 08:54:33 +0530

    LearningProgressUpdateRepository

--- NIGHTLIFE/src/main/java/com/zos/model/LearningProgressUpdateRepository.java
+++ NIGHTLIFE/src/main/java/com/zos/model/LearningProgressUpdateRepository.java
@@ -9,4 +9,3 @@ import java.util.List;
 public interface LearningProgressUpdateRepository extends JpaRepository<LearningProgressUpdate, Long> {
     List<LearningProgressUpdate> findByUser(User user);
 }
-
